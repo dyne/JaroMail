bd35a521206e097978c26c86a4fb7b23d85295c2
Jaromil <jaromil@dyne.org>
2012-06-15 20:51:35 +0200

    fixed osx binary wrapper

--- build/build-osx.sh
+++ build/build-osx.sh
@@ -61,12 +61,12 @@ copydeps() {
 	if ! [ -r build/osx/`basename $1` ]; then
 		# create a wrapper
 		cat <<EOF > build/osx/`basename $1`
-#!/usr/bin/env sh
+#!/usr/bin/env zsh
 test -r \$HOME/Mail/jaro/bin/jaro && PDIR=\$HOME/Mail/jaro
-test -r jaro/bin/jaro && PDIR="`pwd`/jaro"
+test -r jaro/bin/jaro && PDIR="\`pwd\`/jaro"
 test \$JAROMAIL && PDIR=\$JAROMAIL
 DYLD_LIBRARY_PATH=\$PDIR/bin/dylib:\$DYLD_LIBRARY_PATH
-\$PDIR/bin/`basename $1`.bin \$@
+\$PDIR/bin/`basename $1`.bin \${=@}
 EOF
 		chmod +x build/osx/`basename $1`
 	fi
@@ -85,13 +85,22 @@ cd -
 cp src/ABQuery/build/Release/lbdb-ABQuery build/osx/ABQuery
 cd src
 act "Address parser"
-cc -Os -c -m32 fetchaddr.c helpers.c rfc2047.c rfc822.c; \
-cc -Os -m32 -o fetchaddr.32 fetchaddr.o helpers.o rfc2047.o rfc822.o;
-cc -Os -c -m64 fetchaddr.c helpers.c rfc2047.c rfc822.c; \
-cc -Os -m64 -o fetchaddr.64 fetchaddr.o helpers.o rfc2047.o rfc822.o;
+cc -O2 -c -m32 fetchaddr.c helpers.c rfc2047.c rfc822.c; \
+cc -O2 -m32 -o fetchaddr.32 fetchaddr.o helpers.o rfc2047.o rfc822.o;
+cc -O2 -c -m64 fetchaddr.c helpers.c rfc2047.c rfc822.c; \
+cc -O2 -m64 -o fetchaddr.64 fetchaddr.o helpers.o rfc2047.o rfc822.o;
 lipo -arch i386 fetchaddr.32 -arch x86_64 fetchaddr.64 -output fetchaddr -create
 rm -f *.32 *.64
 cd -
+cd src/mairix
+act "Search engine"
+CFLAGS="-O2 -m32" ./configure > /dev/null ; make 2>&1 > /dev/null
+cp mairix mairix.32 && make clean > /dev/null
+CFLAGS="-O2 -m64" ./configure > /dev/null ; make 2>&1 > /dev/null
+cp mairix mairix.64 
+lipo mairix.32 mairix.64 -create -output mairix 2>&1 > /dev/null
+cd -
+
 cp src/fetchaddr build/osx/
 
 copydeps bin/mutt
@@ -100,6 +109,8 @@ copydeps bin/msmtp
 copydeps bin/gpg
 copydeps bin/pinentry
 copydeps bin/lynx
+copydeps libexec/gnubin/find
+copydeps libexec/gnubin/stat
 
 mv build/osx/mutt_dotlock \
    build/osx/dotlock
