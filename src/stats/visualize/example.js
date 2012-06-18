623bd05abbfea61310de481b6aa1eae46bb7aa5b
Jaromil <jaromil@dyne.org>
2012-06-18 15:59:25 +0200

    renamed folder to unsorted.ml, better statistics

--- src/stats/visualize/example.js
+++ src/stats/visualize/example.js
@@ -1,7 +1,5 @@
 // Run the script on DOM ready:
 $(function(){
- $('table').visualize({type: 'pie', height: '300px', width: '420px'});
-	$('table').visualize({type: 'bar', width: '420px'});
-	$('table').visualize({type: 'area', width: '420px'});
-	$('table').visualize({type: 'line', width: '420px'});
+ $('table').visualize({type: 'pie', height: '300px', width: '1024px'});
+	$('table').visualize({type: 'line', width: '1024px'});
 });
