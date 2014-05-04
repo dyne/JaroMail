4432645fa88fb70fb3e0d8938ca2466a6e90ae19
Jaromil <jaromil@dyne.org>
2014-05-04 20:40:56 +0200

    fixed zlibs installation on gnu systems

--- build/install-gnu.sh
+++ build/install-gnu.sh
@@ -18,16 +18,20 @@ mkdir -p $JARO_LIBEXEC
 { test -r $srcdir/src/fetchdate } || {
     print "Error: first build, then install."; return 1 }
 
+mkdir -p $JARO_SHARE/{.procmail,.mutt,.stats}
 cp -ra $srcdir/doc/* $JARO_SHARE/
-cp -ra $srcdir/src/procmail $JARO_SHARE/.procmail
-cp -ra $srcdir/src/mutt $JARO_SHARE/.mutt
-cp -ra $srcdir/src/stats $JARO_SHARE/.stats
+cp -ra $srcdir/src/procmail/* $JARO_SHARE/.procmail/
+cp -ra $srcdir/src/mutt/* $JARO_SHARE/.mutt/
+cp -ra $srcdir/src/stats/* $JARO_SHARE/.stats/
 
 # copy the executables
-mkdir -p $JARO_LIBEXEC/bin
+mkdir -p $JARO_LIBEXEC/{bin,zlibs}
 cp $srcdir/src/jaro $JARO_LIBEXEC/bin
-cp -ra $srcdir/src/zlibs $JARO_LIBEXEC/
 cp -ra $srcdir/build/gnu/* $JARO_LIBEXEC/bin
+cp -ra $srcdir/src/zlibs/* $JARO_LIBEXEC/zlibs/
+for l in `ls $JARO_LIBEXEC/zlibs/ | grep -v '.zwc$'`; do
+    zcompile $JARO_LIBEXEC/zlibs/$l
+done
 
 cat <<EOF > $PREFIX/bin/jaro
 #!/usr/bin/env zsh
